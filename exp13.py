e48b85d7052b1ec043048ef0cf3311e88ffeceb2
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-24 23:22:09 +0530

    Update exp13.py

--- exp13.py
+++ exp13.py
@@ -1,8 +1,8 @@
 //Python program using a for loop to print the numbers from 1 to 15.
-//For statements
-# Initialize a list of numbers
+
+//Initialize a list of numbers
 numbers = [1, 2, 3, 4, 5]
-# Iterate over the list of numbers using a for loop
+//Iterate over the list of numbers using a for loop
 for number in numbers:
   # Print the current number
   print(number)
