2be28b0f3037c18c1a5689d14f83470d26ca1fe0
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-24 23:04:57 +0530

    Create script to print numbers from 1 to 10
    Add a script to check divisibility by 3 and print numbers 1 to 10.

---
+++ exp12.py
@@ -0,0 +1,9 @@
+Execute a script to check whether a number is divisible by 3
+While statements
+# Initialize a variable to keep track of the current number
+i = 1
+
+# While the current number is less than or equal to 10, print the number and increment it by 1
+while i <= 10:
+    print(i)
+    i += 1
