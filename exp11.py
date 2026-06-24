35cf2b4527d415727da5880c5dd00954ec7af885
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-24 23:04:02 +0530

    Add variable type demonstrations in exp11.py

---
+++ exp11.py
@@ -0,0 +1,24 @@
+# Integer
+a = 100
+print(a, type(a))
+# Float
+b = 100.0
+print(b, type(b))
+# String
+c = "100.0"
+print(c, type(c))
+# Boolean
+d = True
+print(d, type(d))
+# List
+e = [1, 2, 3, 4, 5]
+print(e, type(e))
+# Tuple
+f = (1, 2, 3, 4, 5)
+print(f, type(f))
+# Set
+g = {1, 2, 3, 4, 5}
+print(g, type(g))
+# Dictionary
+h = {1: "apple", 2: "banana", 3: "cherry"}
+print(h, type(h))
