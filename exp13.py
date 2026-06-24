01130d12677c187895dce952c483ac719da97114
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-24 23:06:11 +0530

    Add script to print numbers from a list
    Add a script to check divisibility by 3 and print numbers.

---
+++ exp13.py
@@ -0,0 +1,8 @@
+//Execute a script to check whether a number is divisible by 3
+//For statements
+# Initialize a list of numbers
+numbers = [1, 2, 3, 4, 5]
+# Iterate over the list of numbers using a for loop
+for number in numbers:
+  # Print the current number
+  print(number)
