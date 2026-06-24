1a4eac3ad9e67ae6e4993f694c24c71978b3ba86
Neethu Suman <62247880+Neethu-Suman@users.noreply.github.com>
2026-06-24 23:20:38 +0530

    Update exp12.py

--- exp12.py
+++ exp12.py
@@ -1,6 +1,6 @@
-Execute a script to check whether a number is divisible by 3
-While statements
-# Initialize a variable to keep track of the current number
+//Python program using a while loop to print the numbers from 1 to 10.
+
+//Initialize a variable to keep track of the current number
 i = 1
 
 # While the current number is less than or equal to 10, print the number and increment it by 1
